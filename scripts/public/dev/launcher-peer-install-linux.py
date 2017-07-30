9cdd506a427245a841cd3decf8d75f45bd39fd3e
Mikhail Savochkin <i@crioto.com>
2017-07-31 01:13:01 +0600

    Script fixes

--- scripts/public/dev/launcher-peer-install-linux.py
+++ scripts/public/dev/launcher-peer-install-linux.py
@@ -177,11 +177,8 @@ def setupVm(machineName):
         while subutai.isDownloadComplete() != 1:
             sleep(0.05)
         subutai.VBox("import " +
-                     subutai.GetTmpDir().replace(" ", "+++") + "core.ova")
+                     subutai.GetTmpDir().replace(" ", "+++") + "core.ova --vsys 0 --vmname " + machineName)
         sleep(10)
-        ret = subutai.VBoxS("modifyvm core --name " + machineName)
-        if ret != 0:
-            return 1
 
         cpus = subutai.GetCoreNum()
         mem = subutai.GetMemSize() * 1024
