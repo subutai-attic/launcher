ccddfba3e226c772eb2310e14fd43a4bb490caac
Mikhail Savochkin <i@crioto.com>
2017-07-30 10:40:02 +0600

    Build updated to fix issues on Linux

--- package/linux/pack.sh
+++ package/linux/pack.sh
@@ -14,11 +14,16 @@ fi
 
 rm -rf debian/python
 rm -rf debian/SubutaiLauncher
+rm -rf debian/subutai-launcher
+#rm -rf debian/poco
 
 mkdir -p debian/python
+#mkdir -p debian/poco
 
 cp $launcher debian/SubutaiLauncher
+#cp ../../bin/libsubutai-launcher.so debian/libsubutai-launcher.so
 cp -r $pythondir/* debian/python/
+#cp -r /usr/local/lib/libPoco*.so* debian/poco
 
 # TODO: Add changelog converter
 # TODO: Add build scheme
