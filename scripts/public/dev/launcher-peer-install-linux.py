20a491875424379f6276f405190b1fcffdc06b64
Mikhail Savochkin <i@crioto.com>
2017-07-31 16:05:02 +0600

    Fixed linux peer installation

--- scripts/public/dev/launcher-peer-install-linux.py
+++ scripts/public/dev/launcher-peer-install-linux.py
@@ -163,8 +163,8 @@ def startVm(machineName):
 def stopVm(machineName):
     subutai.SSHRun("sync")
     subutai.log("info", "Stopping Virtual machine")
-    if subutai.CheckVMRunning(machineName) != 0:
-        subutai.VBox("controlvm " + machineName + " poweroff soft")
+    #if subutai.CheckVMRunning(machineName) != 0:
+    subutai.VBox("controlvm " + machineName + " poweroff soft")
 
     return
 
