a782f6c44210520185e5c7f61b5646a8d14e56af
Mikhail Savochkin <i@crioto.com>
2017-08-01 12:15:22 +0600

    UI improvements

--- UI/Source/HubComponent.cpp
+++ UI/Source/HubComponent.cpp
@@ -17,7 +17,7 @@ void HubComponent::paint(Graphics& g)
 {
     g.setFont(Font("Encode Sans", 23, 1));
     g.setColour (Colour::fromRGB(105, 116, 144));
-    g.drawText ("We are launching soon!", Rectangle<int>(0, getHeight() / 2 + 115, getWidth(), 40), Justification::centred, true);
+    g.drawText ("We are launching soon!", juce::Rectangle<int>(0, getHeight() / 2 + 115, getWidth(), 40), Justification::centred, true);
 }
 
 void HubComponent::resized() 
