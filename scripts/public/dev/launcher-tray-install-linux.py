9c2635b11cc2d8744dd6c506edaa8b940f66fed9
Mikhail Savochkin <i@crioto.com>
2017-08-03 03:02:04 +0600

    Improvements in P2P and Tray installation scripts

--- scripts/public/dev/launcher-tray-install-linux.py
+++ scripts/public/dev/launcher-tray-install-linux.py
@@ -1,6 +1,7 @@
 import subutai
 import tarfile
 from time import sleep
+from subprocess import call
 
 
 def subutaistart():
@@ -23,6 +24,26 @@ def subutaistart():
 
     subutai.AddStatus("Creating Symlink")
     subutai.MakeLink(installDir+"/bin/SubutaiTray", "/usr/local/bin/SubutaiTray")
+
+    desktop = '''
+[Desktop Entry]
+Version=1.0
+Name=Subutai Tray
+Comment=Subutai Tray
+Keywords=Internet;
+Exec=SubutaiTray
+Terminal=false
+Type=Application
+Icon=/usr/share/icons/launcher-logo.png
+Categories=GNOME;Network;
+StartupNotify=true
+    '''.strip()
+
+    f = open(tmpDir+"SubutaiTray.desktop", 'w')
+    f.write(desktop)
+    f.close()
+
+    subutai.DesktopFileInstall(tmpDir+"SubutaiTray.desktop")
 
     subutai.Shutdown()
 
